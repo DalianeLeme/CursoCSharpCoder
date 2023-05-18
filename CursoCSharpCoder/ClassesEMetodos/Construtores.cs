d1c3cca4ff25c1879a250e7d307b26e6a2a7a35c
Daliane <daliane.leme@hotmail.com>
2023-05-18 09:10:09 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharpCoder/ClassesEMetodos/Construtores.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Construtores
+    {
+        class Carro
+        {
+            public string Modelo;
+            public string Fabricante;
+            public int Ano;
+
+            public Carro(string modelo, string fabricante, int ano)
+            {
+                Modelo = modelo;
+                Fabricante = fabricante;
+                Ano = ano;
+            }
+
+            public Carro()
+            {
+                //Construtor Padrão
+            }
+        }
+
+        public static void Executar()
+        {
+            var carro1 = new Carro();
+            carro1.Modelo = "Ford Ka";
+            carro1.Fabricante = "Ford";
+            carro1.Ano = 2022;
+            Console.WriteLine($"{carro1.Modelo} {carro1.Fabricante} {carro1.Ano}");
+
+            var carro2 = new Carro("1234", "BMW", 2015);
+            Console.WriteLine($"{carro2.Modelo} {carro2.Fabricante} {carro2.Ano}");
+
+            var carro3 = new Carro()
+            {
+                Fabricante = "Fiat",
+                Modelo = "Uno",
+                Ano = 2007
+            };
+            Console.WriteLine($"{carro3.Modelo} {carro3.Fabricante} {carro3.Ano}");
+        }
+    }
+}
---
+++ CursoCSharpCoder/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+using CursoCSharp.ClassesEMetodos;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                // Fundamentos
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários - Fundamentos", Comentarios.Executar},
+                {"Váriaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
+                {"Inferência - Fundamentos", Inferencia.Executar},
+                {"Interpolação - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
+                {"Formatando Número - Fundamentos", FormatandoNumero.Executar},
+                {"Conversões - Fundamentos", Conversoes.Executar},
+                {"Operadores Aritméticos - Fundamentos", OperadoresAritmeticos.Executar},
+                {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
+                {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
+                {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
+                {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar},
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
+                // Estruturas de Controle
+                {"Estrutura If - Estruturas de Controle", EstruturaIf.Executar},
+                {"Estrutura If/Else - Estruturas de Controle", EstruturaIfElse.Executar},
+                {"Estrutura If/Else If - Estruturas de Controle", EstruturaIfElseIf.Executar},
+                {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
+                {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar},
+                {"Estrutura Do While - Estruturas de Controle", EstruturaDoWhile.Executar},
+                {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
+                {"Estrutura Foreach - Estruturas de Controle", EstruturaForEach.Executar},
+                {"Usando Break - Estruturas de Controle", UsandoBreak.Executar},
+                {"Usando Continue - Estruturas de Controle", UsandoContinue.Executar},
+                // Classes e Métodos
+                {"Membros - Classes e Metodos", Membros.Executar},
+                {"Construtores - Classes e Metodos", Construtores.Executar},
+                {"Métodos com Retorno - Classes e Metodos", MetodosComRetorno.Executar},
+                {"Métodos Estáticos - Classes e Metodos", MetodosEstaticos.Executar},
+                {"Atributos Estáticos - Classes e Metodos", AtributosEstaticos.Executar},
+                {"Desafio Atributo - Classes e Metodos", DesafioAtributo.Executar},
+                {"Parametros Variáveis - Classes e Metodos", Params.Executar},
+                {"Parametros Nomeados - Classes e Metodos", ParametrosNomeados.Executar},
+                {"Getters e Setters - Classes e Metodos", GetSet.Executar},
+                {"Propriedades - Classes e Metodos", Props.Executar},
+                {"Readonly - Classes e Metodos", Readonly.Executar},
+                {"Enum - Classes e Metodos", ExemploEnum.Executar},
+                {"Struct - Classes e Metodos", Struct.Executar},
+                {"Struct Vs Classe - Classes e Metodos", StructVsClasse.Executar},
+                {"Valor Vs Referência - Classes e Metodos", ValorVsReferencia.Executar},
+                {"Parametros por Referência - Classes e Metodos", ParamentrosPorReferencia.Executar},
+                {"Parametros por valor Padrão - Classes e Metodos", ParametroPadrao.Executar},
+                // Coleções
+                {"Array - Coleções", Colecoes.Array.Executar},
+                {"List - Coleções", ColecoesList.Executar},
+                {"ArrayList - Coleções", ColecoesArrayList.Executar},
+                {"Set - Coleções", ColecoesSet.Executar},
+                {"Queue - Coleções", ColecoesQueue.Executar},
+                {"Igualdade - Coleções", Igualdade.Executar},
+                {"Stack - Coleções", ColecoesStack.Executar},
+                {"Dictionary - Coleções", ColecoesDictionary.Executar},
+                // OO
+                {"Herança - OO", Heranca.Executar},
+                {"Construtor This - OO", ConstrutorThis.Executar},
+                //{"Encapsulamento - OO", OO.Encapsulamento.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
