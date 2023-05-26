4a171638dee7510ae1250212a18fad8879d96f0c
Daliane <daliane.leme@hotmail.com>
2023-05-25 22:29:33 -0300

    feat: estudos de classes e métodos sealed

--- CursoCSharpCoder/Program.cs
+++ CursoCSharpCoder/Program.cs
@@ -73,6 +73,7 @@ namespace CursoCSharp
                 {"Polimorfismo - OO", Polimorfismo.Executar},
                 {"Abstract - OO", Abstract.Executar},
                 {"Interface - OO", Interface.Executar},
+                {"Sealed - OO", Sealed.Executar},
             });
 
             central.SelecionarEExecutar();
